8ccc29c17d8aaf4e2bca01b52c67e8e3945cadad
Zan Hong <mokzanhong92@hotmail.com>
2020-09-26 18:47:09 +0930

    added jumbotron header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Wrapper from "./components/Wrapper/Wrapper";
 import Table from "./components/Table/Table";
 import employees from "./employees.json";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
 class App extends React.Component {
   state = {
@@ -19,6 +21,14 @@ class App extends React.Component {
 
     return (
       <Wrapper>
+        <Jumbotron fluid>
+          <Container>
+            <h1>Employee Directory</h1>
+            <p>
+              Click on the table headers to sort them ascendingly or descendingly.
+            </p>
+          </Container>
+        </Jumbotron>
         <Table employees={this.state.employees} handleSortClick={this.handleSortClick} />
       </Wrapper>
     );
