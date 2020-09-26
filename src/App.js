cb29b1d081c2801673442478ead0075a4e60ef55
Zan Hong <mokzanhong92@hotmail.com>
2020-09-26 21:57:03 +0930

    added search form functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Wrapper from "./components/Wrapper/Wrapper";
 import EmployeeTable from "./components/EmployeeTable/EmployeeTable";
 import SearchForm from "./components/SearchForm/SearchForm";
-import employees from "./employees.json";
+import Employees from "./employees.json";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 
 
 class App extends React.Component {
   state = {
-    employees: employees,
-    result: []
+    employees: Employees,
+    search: ""
   }
 
   handleSortClick = event => {
@@ -18,21 +18,80 @@ class App extends React.Component {
     console.log("click")
   }
 
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+
+    const employees = this.state.employees;
+    let query = this.state.search;
+
+    console.log("Query: " + query);
+
+    const searchEmployee = employees.filter(result => result.name.toLowerCase().includes(query.toLowerCase()))
+
+    if (searchEmployee.length === 0) {
+      // When no match is found
+      console.log("No match");
+      this.setState({
+        employees: Employees,
+        search: ""
+      });
+      alert("No match found! Please try again!");
+
+    } else if (query === "") {
+      // When no input is detected
+      console.log("No query entered");
+      this.setState({
+        employees: Employees,
+        search: ""
+      });
+      alert("No query entered! Please try again!");
+
+    } else {
+      console.log(searchEmployee);
+      this.setState({
+        employees: searchEmployee,
+        search: ""
+      })
+    }
+  }
+
+
   render() {
-    console.log(this.state.employees);
 
     return (
       <Wrapper>
         <Jumbotron fluid>
           <Container>
             <h1>Employee Directory</h1>
-            <p>
-              Click on the table headers (Name, Occupation and Location) to sort them ascendingly or descendingly.
-            </p>
+            <h5>How to use?</h5>
+            <ol>
+              <li>Enter a name to search employee OR</li>
+              <li>Click on the table headers (Name, Occupation and Location) to sort them ascendingly or descendingly.</li>
+              <li>To reset search list, simply submit the form again to reload the full list</li>
+            </ol>
           </Container>
         </Jumbotron>
-        <SearchForm />
-        <EmployeeTable employees={this.state.employees} handleSortClick={this.handleSortClick} />
+        <SearchForm
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+        <EmployeeTable
+          employees={this.state.employees}
+          handleSortClick={this.handleSortClick}
+        />
       </Wrapper>
     );
   }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,13 +3,25 @@ import "./style.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-function SearchForm() {
+function SearchForm(props) {
   return (
     <Form inline>
       <Form.Group controlId="formBasicEmail" className="mx-md-2">
         <Form.Label className="mx-md-2">Search By Name: </Form.Label>
-        <Form.Control type="email" placeholder="Enter name" className="mx-md-2" />
-        <Button variant="primary" type="submit" className="mx-md-2">
+        <Form.Control
+          type="text"
+          placeholder="Enter name"
+          className="mx-md-2"
+          name="search"
+          value={props.value}
+          onChange={props.handleInputChange}
+        />
+        <Button
+          variant="primary"
+          type="submit"
+          className="mx-md-2"
+          onClick={props.handleFormSubmit}
+        >
           Submit
         </Button>
       </Form.Group>
